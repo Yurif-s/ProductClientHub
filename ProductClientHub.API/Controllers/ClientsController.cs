dcb23c81185c349c718af81362cc016ce89867a6
Yuri <yurimfs30@gmail.com>
2024-12-11 20:16:05 -0300

    initial commit

---
+++ ProductClientHub.API/Controllers/ClientsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClientHub.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ClientsController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register()
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpPut]
+    public IActionResult Put()
+    {
+        return Ok();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete()
+    {
+        return Ok();
+    }
+}
