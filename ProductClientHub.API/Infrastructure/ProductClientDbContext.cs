74f129f085ba084c18010c7a2c337aa6fc43d2b9
Yuri <yurimfs30@gmail.com>
2024-12-16 15:38:25 -0300

    feat: Config Entity Framework

---
+++ ProductClientHub.API/Infrastructure/ProductClientDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductClientHub.API.Entities;
+
+namespace ProductClientHub.API.Infrastructure;
+
+public class ProductClientDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Workspace\\ProductClientHubDB\\ProductClientHub.db");
+    }
+}
